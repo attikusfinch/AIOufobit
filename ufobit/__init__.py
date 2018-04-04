15758921c495c0d1d366c61f05f75b6b294aa31a
banteg <banteeg@gmail.com>
2018-04-04 15:42:52 +0700

    bump version

--- ufobit/__init__.py
+++ ufobit/__init__.py
@@ -4,4 +4,4 @@ from ufobit.network.rates import SUPPORTED_CURRENCIES, set_rate_cache_time
 from ufobit.network.services import set_service_timeout
 from ufobit.wallet import Key, PrivateKey, wif_to_key
 
-__version__ = '0.4.3'
+__version__ = '0.4.4'
