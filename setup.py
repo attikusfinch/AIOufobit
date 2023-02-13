830348e10cc5f7455d033dc9944b66905ef69c34
amirmistik <57581726+amirmistikad@users.noreply.github.com>
2023-02-13 17:10:42 +0500

    edit setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     ),
 
     classifiers=(
-        'Development Status :: 4 - Beta',
+        'Development Status :: 8 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
